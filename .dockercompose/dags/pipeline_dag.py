96ce9c2f7aa23dfe33ad26f8148607783ce30f44
Bruno Gabriel <34722056+brunogcs@users.noreply.github.com>
2026-03-29 00:11:08 +0000

    update infra

--- .dockercompose/dags/pipeline_dag.py
+++ .dockercompose/dags/pipeline_dag.py
@@ -1,13 +1,13 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 import subprocess
 
 def run_pipeline():
     # Executa seu script Python existente
-    subprocess.run(["python3", "/workspaces/Projeto 1 - Engenharia de Dados/scripts/pipeline.py"], check=True)
+    subprocess.run(["python3", "/home/brunoserver/projetos/data-engineering-lab/scripts/pipeline.py"], check=True)
 
-dag = DAG('data_pipeline', start_date=datetime(2026, 3, 7), schedule_interval=None)
+dag = DAG('data_pipeline', start_date=datetime(2026, 3, 7), schedule=None)
 
 task = PythonOperator(
     task_id='run_pipeline',
